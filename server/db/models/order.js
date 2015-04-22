bd703a465ef320ab78c64192e72a3ddf17e40f0f
Peege151 <Pjsulli2k@gmail.com>
2015-04-22 11:25:16 -0400

    fix mongoose bug

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -17,9 +17,7 @@ var lineItemSchema = new mongoose.Schema({
 });
 
 var schema = new mongoose.Schema({
-	products: [{
-        type: lineItemSchema
-    }],
+	products: [lineItemSchema],
     user: {
         type: mongoose.Schema.Types.ObjectId, ref: 'User'
     },
