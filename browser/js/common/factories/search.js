3e94249a8417522564760fb04838145bcd0e8753
Tony <terimyaki@users.noreply.github.com>
2015-04-25 14:54:05 -0400

    the search works. it can handle wildcard search on product and store name. Also can search by partial product name by store

--- browser/js/common/factories/search.js
+++ browser/js/common/factories/search.js
@@ -5,7 +5,7 @@ app.factory('SearchFactory', function ($http) {
 	return{
 		searchProductByStore : function(query, storeID){
 			var config = {
-				params : query
+				params : {term : query}
 			};
 			return $http.get('/api/search/' + storeID, config).then(function(response){
 				console.log(response.data);
@@ -14,7 +14,7 @@ app.factory('SearchFactory', function ($http) {
 		},
 		searchWildCard : function(query){
 			var config = {
-				params : query
+				params : {term : query}
 			};
 			return $http.get('/api/search', config).then(function(response){
 				console.log(response.data);
