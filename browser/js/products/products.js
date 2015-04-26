693df78dfe29927476817a852d216c52cfb41146
Ian Downie <iandownie@users.noreply.github.com>
2015-04-25 21:33:05 -0400

    Revert "shopping cart populates/refreshes correctly"

--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -85,7 +85,7 @@ app.controller('ProductCtrl', function ($scope, $state, AuthService, productsInf
 
     $scope.addCart = function(product, quant){
         NavFactory.loader=false;
-        CartFactory.addToCart(product, quant);
+        CartFactory.addCart(product, quant);
         NavFactory.loader=true;
 
     };
