fda18521f75e09b3c93dd1ffa55e1204157612d1
Peege151 <Pjsulli2k@gmail.com>
2015-04-28 19:20:15 -0400

    facebook login halfway working, users can log-in.  But when they are created they don't get assigned the proper names etc'

--- server/env/development.js
+++ server/env/development.js
@@ -7,9 +7,9 @@ module.exports = {
     "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
   },
   "FACEBOOK": {
-    "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
-    "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+    "clientID": "571425439626513",
+    "clientSecret": "67a2864863e31206f148617885dfa291",
+    "callbackURL": "http://localhost:3333/auth/facebook/callback"
   },
   "GOOGLE": {
     "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
