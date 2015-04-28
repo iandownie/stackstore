a2e5d7d2d1153b2874afe507a84af9cefcec3e5c
Tony <terimyaki@users.noreply.github.com>
2015-04-28 16:21:26 -0400

    it can loadStoreFront now

--- browser/js/stores/stores.js
+++ browser/js/stores/stores.js
@@ -6,24 +6,6 @@ app.config(function ($stateProvider) {
         controller: 'StoresController',
         templateUrl: 'js/stores/stores.html',
     });
-
-    $stateProvider.state('storeFront', {
-        url: '/stores/:url',
-        controller: 'StoreFrontController',
-        templateUrl: 'js/stores/store-front.html',
-        resolve: {
-            getStoreByUrl: function($stateParams, $state, StoresFactory){
-                console.log($stateParams.url);
-                return StoresFactory.loadStoreFrontByUrl($stateParams.url).catch(function(err){
-                    $state.go('error');
-                });
-            },
-            categoryList: function(CategoryFactory){
-                return CategoryFactory.getCategoryList();
-            }
-        }
-    });
-
 });
 
 app.controller('StoresController', function ($state, $scope, StoresFactory) {
@@ -33,85 +15,6 @@ app.controller('StoresController', function ($state, $scope, StoresFactory) {
         })
         .catch(function (err){
         });
-});
-
-//app.controller('StoreFrontController', function ($state, $scope, $http, AuthService, StoresFactory, getStoreById, categoryList, CategoryFactory) {
-
-app.controller('StoreFrontController', function ($state, $scope, $http, AuthService, NavFactory, StoresFactory, getStoreByUrl, categoryList, CategoryFactory, experimentalFactory, ProductFactory) {
-    $scope.currentStore = getStoreByUrl;
-
-    $scope.categoryList = categoryList;
-    //proof of concept of adding categories
-    $scope.categoryName = '';
-    $scope.currentUser = null;
-
-    $scope.product = {
-        name: undefined,
-        price: undefined,
-        quantity: undefined,
-        description: undefined,
-        store: undefined,
-        categories: undefined,
-        images : undefined
-    };
-
-    $scope.selectedProduct=$scope.currentStore.products[0];
-    $scope.sortType     = 'name'; // set the default sort type
-    $scope.sortReverse  = false;  // set the default sort order
-
-    AuthService.getLoggedInUser().then(function (user) {
-        $scope.currentUser = user;
-        $scope.product.store = user.store;
-
-    });
-
-    $scope.isOwner = function(){
-        return StoresFactory.isOwner($scope.currentUser._id, $scope.currentStore.user._id);
-    };
-    $scope.editProduct=function(data){
-        NavFactory.loader=false;
-        ProductFactory.editProduct(data).then(function (respeonse){
-            $state.go('storeFront', {url: $scope.currentStore.url }, {reload: true});
-            NavFactory.loader=true;
-        })
-    }
-    $scope.deleteProduct=function(data){
-        NavFactory.loader=false;
-        ProductFactory.deleteProduct(data).then(function (response){
-            $state.go('storeFront', {url: $scope.currentStore.url }, {reload: true});
-           NavFactory.loader=true;
-        });
-    }
-    $scope.newProduct =function(data){
-        NavFactory.loader=false;
-        StoresFactory.newProduct(data).then(function (response){
-            $state.go('storeFront', {url: $scope.currentStore.url }, {reload: true});
-           NavFactory.loader=true;
-        });
-    };
-
-    $scope.loadStoreFront = function(storeUrl, categories){
-        NavFactory.loader=false;
-        StoresFactory.loadStoreFrontByUrl(storeUrl, categories).then(function(data){
-            $scope.currentStore = data;
-           NavFactory.loader=true;
-        });
-    };
-
-    $scope.addCategory = function(category){
-        NavFactory.loader=false;
-        CategoryFactory.addCategory(category).then(function(data){
-            $state.go('storeFront', {url: $scope.currentStore.url }, {reload: true});
-           NavFactory.loader=true;
-        });
-    };
-    $scope.customizeStore =function(properties){
-        NavFactory.loader=false;
-        experimentalFactory.writeAFile(properties).then(function(){
-            $state.go('storeFront', {id:$scope.currentStore._id},{reload:true});
-        NavFactory.loader=true;
-        });
-    };
 });
 
 app.factory('StoresFactory', function ($http) {
@@ -132,15 +35,14 @@ app.factory('StoresFactory', function ($http) {
                 });
         },
         loadStoreFrontByUrl: function(storeUrl, categories){
-            console.log("storeUrl: ", storeUrl)
-            console.log("categories: ", categories)
             var config = {
                 params : {categories: categories}
             };
             return $http.get('/api/stores/' + storeUrl, config)
                 .then(function(response){
-                    console.log("data: ", response.data)
-                    return response.data;
+                    //object with two keys shows up
+                    response.data.store.products = response.data.products;
+                    return response.data.store;
                 });
         },
         newProduct: function(data){
@@ -151,17 +53,6 @@ app.factory('StoresFactory', function ($http) {
         },
         isOwner : function(userID, ownerID){
             return userID === ownerID;
-        }
-    };
-});
-
-app.factory('experimentalFactory', function($http){
-    return {
-        writeAFile: function(properties){
-            return $http.post("/api/experiment/", properties)
-            .then(function(response){
-                return response.data;
-            });
         }
     };
 });
